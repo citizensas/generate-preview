db0ff3b0b882ebc1618e4b1db7bf8010452ef953
Sassoun Derderian <sassounderderian@workfront.com>
2020-04-16 15:32:05 +0400

    feat: pass a custom branch name with the "-b" flag

--- src/getBranchName.ts
+++ src/getBranchName.ts
@@ -1,11 +1,17 @@
 import * as gitP from 'simple-git/promise'
 import {MODULE_DIR} from './constants'
 import {logger} from './logger'
+import {getFlags} from './cliFlags'
 
 const moduleGit = gitP(MODULE_DIR)
 
 export function getBranchName() {
+    const {branchName} = getFlags()
     logger.verbose('Getting branch name')
+    if (branchName) {
+        logger.info(`Branch name: ${branchName}`)
+        return Promise.resolve(branchName)
+    }
     return moduleGit
         .branchLocal()
         .then(branchSummary => branchSummary.current)
